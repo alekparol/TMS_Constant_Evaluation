de670ffa06624b9615e6ab99dc60c405995d2ef2
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-05-21 13:12:27 +0200

    Adding Status and Planning Classes

---
+++ TMS_Constant_Evaluation/TMS_Constant_Evaluation/PM_Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*TODO: Format validation for languages, emails, job names,*/
+
+namespace TMS_Constant_Evaluation
+{
+    class PM_Data
+    {
+
+        /* Fields */
+
+        private string sourceLanguage;
+        private string targetLanguage;
+
+        private string translatorName;
+        private string translatorEmail;
+
+        private string editorName;
+        private string editorEmail;
+
+        private string jobName;
+        private int totalWordCount;
+        private string deadline;
+
+        /* Properties */
+
+        /* Methods */
+
+        /* Constructors */
+
+    }
+}
---
+++ TMS_Constant_Evaluation/TMS_Constant_Evaluation/PlanningPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TMS_Constant_Evaluation
+{
+    class PlanningPage
+    {
+
+        /* Fields */
+
+        private IWebElement planningTitle; // This is title of the page to validate if the page was displayed properly.
+
+        private IWebElement filtersButton;
+        private IWebElement activityList;
+        private IWebElement statusList;
+
+
+
+        /* Constructors */
+
+        /* filtersButton //button[contains(text(),'Filters')]
+         *  after clicking the bar should be displayed - it should be validated by checking if next element "Activity" is displayed. 
+         *  It can be checked <input type="hidden" name="cup$display" id="cup_display" value="display:none"> - when the Filters is not clicked. 
+         *  <input type="hidden" name="cup$display" id="cup_display" value=""> if is. 
+         *  activityList //*[@id='cup_fpStepActivityName_titletext']
+         *  statusList //*[@id='cup_fpStatusId_titletext']
+         *  
+         *  After clicking on activityList there will be dropDown menu displayed. Each element has an id cup_fpStepActivityName_msa_N, where N 
+         *  is an index of the option. There should be created list there to check it with a regex expression. Also cup_fpStepActivityName_msa_N is 
+         *  an option with text the same as the field.
+         * */
+
+    }
+}
