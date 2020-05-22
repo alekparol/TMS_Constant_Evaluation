d472a63e95debc0cde20cd5bfc9ff585215abd9a
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-05-22 14:06:44 +0200

    Adding test in which we are filter the page with InternalReview activity

---
+++ TMS_Constant_Evaluation/TMS_Constant_Evaluation/Pages/PagesObjects/StatusObject.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Constant_Evaluation
+{
+    public class StatusObject
+    {
+        /* Fields */
+
+        public string jobName;
+
+        public string sourceTargetLanguages;
+        public string jobStatus;
+
+        public int stepsCount;
+
+        public IWebElement moreInfo;
+       
+        /* Properties */
+
+        /* Methods */
+
+        /* Constructors */
+
+        /*
+         * jobName = class="grp_ttl"
+         * sourceTargetLanguages = class="grp_lg"
+         * jobStatus = class="grp_aty_501 tlp_on title="
+         **/
+
+        public StatusObject()
+        {
+            jobName = string.Empty;
+
+            sourceTargetLanguages = string.Empty;
+            jobStatus = string.Empty;
+
+            stepsCount = 0;
+
+            moreInfo = null;
+
+        }
+
+        public StatusObject(IWebElement statusObjectTitle)
+        {
+
+            jobName = statusObjectTitle.Text;
+
+            sourceTargetLanguages = statusObjectTitle.FindElement(By.XPath("..")).FindElement(By.ClassName("grp_lg")).Text;
+            jobStatus = statusObjectTitle.FindElement(By.XPath("..")).FindElement(By.ClassName("grp_aty_501")).Text;
+
+            Int32.TryParse(statusObjectTitle.FindElement(By.XPath("../../..")).FindElement(By.ClassName("r_LCount")).Text,out stepsCount);
+
+
+        }
+
+    }
+}
--- TMS_Constant_Evaluation/TMS_Constant_Evaluation_Tests/StatusPage_Tests.cs
+++ TMS_Constant_Evaluation/TMS_Constant_Evaluation_Tests/StatusPage_Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -12,7 +14,7 @@ namespace TMS_Constant_Evaluation_Tests
     {
         [TestMethod]
         public void StatusPage_Initialization_Test_1()
-        {           
+        {
             using (var driver = new ChromeDriver())
             {
                 // Initialization
@@ -36,11 +38,127 @@ namespace TMS_Constant_Evaluation_Tests
 
                 IWebElement statusPage = driver.FindElement(By.XPath("//li[@id='status']"));
                 statusPage.Click();
-                
+
                 wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='cup_cupavFalse']")));
-                
+
                 StatusPage objectStatusPage = new StatusPage(driver);
                 Assert.AreEqual("Status", objectStatusPage.PageTitle.Text);
+
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void StatusPage_Initialization_Test_2()
+        {           
+            using (var driver = new ChromeDriver())
+            {
+                // Initialization
+                List<StatusObject> listStatusObjects = new List<StatusObject>();
+                StatusObject auxiliaryStatusObject = new StatusObject();
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Porsche BAL 2.0";
+                string projectXPath = "//div[contains(text(),'" + projectTitle + "')]";
+
+                // Waiting for the Porsche BAL 2.0 project element which exists only for added users
+                wait.Until(ExpectedConditions.ElementExists(By.XPath(projectXPath)));
+                IWebElement projectTMS = driver.FindElement(By.XPath(projectXPath));
+
+                projectTMS.Click();
+
+                // Waiting for the "Status" section's element which exists only in the particular project
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//li[@id='status']")));
+                IWebElement statusPage = driver.FindElement(By.XPath("//li[@id='status']"));
+                statusPage.Click();
+                
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='cup_cupavFalse']")));                
+                StatusPage objectStatusPage = new StatusPage(driver);
+
+                IReadOnlyCollection<IWebElement> listOfTitlesStatusWebElements = driver.FindElements(By.ClassName("grp_ttl"));
+                Assert.AreEqual("RedSys - X171702_4440095", listOfTitlesStatusWebElements.ElementAt(0).Text);
+                Assert.AreEqual("de-de → ar-ae", listOfTitlesStatusWebElements.ElementAt(0).FindElement(By.XPath("..")).FindElement(By.ClassName("grp_lg")).Text);
+
+                if(listOfTitlesStatusWebElements.Count != 0)
+                {
+                    foreach(IWebElement element in listOfTitlesStatusWebElements)
+                    {
+                        auxiliaryStatusObject = new StatusObject(element);
+                        listStatusObjects.Add(auxiliaryStatusObject);
+                    }                    
+                }
+
+      
+                Assert.AreNotEqual(12, listStatusObjects[0].stepsCount);
+                Assert.AreEqual(21, listStatusObjects.Count);
+
+                objectStatusPage.ActivityFilter.Click();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("cup_fpStepActivityName_msa_0")));
+
+                IWebElement internalReviewActivity = objectStatusPage.activityList.Where(x => x.Text == "InternalReview").ElementAt(0);
+                internalReviewActivity.Click();
+
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void StatusPage_Initialization_Test_3()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                // Initialization
+                List<StatusObject> listStatusObjects = new List<StatusObject>();
+                StatusObject auxiliaryStatusObject = new StatusObject();
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Porsche BAL 2.0";
+                string projectXPath = "//div[contains(text(),'" + projectTitle + "')]";
+
+                // Waiting for the Porsche BAL 2.0 project element which exists only for added users
+                wait.Until(ExpectedConditions.ElementExists(By.XPath(projectXPath)));
+                IWebElement projectTMS = driver.FindElement(By.XPath(projectXPath));
+
+                projectTMS.Click();
+
+                // Waiting for the "Status" section's element which exists only in the particular project
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//li[@id='status']")));
+                IWebElement statusPage = driver.FindElement(By.XPath("//li[@id='status']"));
+                statusPage.Click();
+
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id='cup_cupavFalse']")));
+                StatusPage objectStatusPage = new StatusPage(driver);
+
+                IReadOnlyCollection<IWebElement> listOfTitlesStatusWebElements = driver.FindElements(By.ClassName("grp_ttl"));
+                Assert.AreEqual("RedSys - X171702_4440095", listOfTitlesStatusWebElements.ElementAt(0).Text);
+                Assert.AreEqual("de-de → ar-ae", listOfTitlesStatusWebElements.ElementAt(0).FindElement(By.XPath("..")).FindElement(By.ClassName("grp_lg")).Text);
+
+                if (listOfTitlesStatusWebElements.Count != 0)
+                {
+                    foreach (IWebElement element in listOfTitlesStatusWebElements)
+                    {
+                        auxiliaryStatusObject = new StatusObject(element);
+                        listStatusObjects.Add(auxiliaryStatusObject);
+                    }
+                }
+
+                objectStatusPage.ActivityFilter.Click();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("cup_fpStepActivityName_msa_0")));
+
+                IWebElement internalReviewActivity = objectStatusPage.activityList.Where(x => x.Text == "InternalReview").ElementAt(0);
+                internalReviewActivity.Click();
+
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("cup_lod")));
+                wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.Id("cup_lod")));
+                //wait.Until(ExpectedConditions.ElementExists(By.Id("cup_fpStepActivityName_msa_0")));
 
                 driver.Quit();
             }
