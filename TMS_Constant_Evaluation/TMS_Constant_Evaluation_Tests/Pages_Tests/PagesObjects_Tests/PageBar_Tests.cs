a306ec1bc3c2fb527db19303893808603a3895a2
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-07-16 17:39:10 +0200

    Adding and fixing tests

--- TMS_Constant_Evaluation/TMS_Constant_Evaluation_Tests/Pages_Tests/PagesObjects_Tests/PageBar_Tests.cs
+++ TMS_Constant_Evaluation/TMS_Constant_Evaluation_Tests/Pages_Tests/PagesObjects_Tests/PageBar_Tests.cs
@@ -641,6 +641,117 @@ namespace TMS_Constant_Evaluation_Tests.Pages_Tests.PagesObjects_Tests
             }
         }
 
+        /* GoToLastPage Tests */
+
+        [TestMethod]
+        public void PageBar_GoToLastPage_Test_1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(testPageBar.GetLastPageNumber, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(0, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(1, testPageBar.NextPageIsNull);
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToLastPage_Test_2()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                qlikStatusPage.ActivityFilterClick();
+                qlikStatusPage.ChosenActivityClick("Buffer_for_FreewayReview_H", driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(-1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(-1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(-1, testPageBar.NextPageIsNull);
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToLastPage_Test_3()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://lionbridge.com/");
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(-1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(-1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(-1, testPageBar.NextPageIsNull);
+            }
+        }
+
         /* GoToNextPage Tests */
 
         [TestMethod]
@@ -834,7 +945,330 @@ namespace TMS_Constant_Evaluation_Tests.Pages_Tests.PagesObjects_Tests
             }
         }
 
+        /* GoToFirstPage Tests */
+
+        [TestMethod]
+        public void PageBar_GoToFirstPage_Test_1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                testPageBar.GoToFirstPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToFirstPage_Test_2()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                qlikStatusPage.ActivityFilterClick();
+                qlikStatusPage.ChosenActivityClick("Buffer_for_FreewayReview_H", driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                testPageBar.GoToFirstPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(-1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(-1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(-1, testPageBar.NextPageIsNull);
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToFirstPage_Test_3()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://lionbridge.com/");
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToFirstPage(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(-1, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(-1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(-1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(-1, testPageBar.NextPageIsNull);
+            }
+        }
+
         /* GoToPreviousPage Tests */
+
+        [TestMethod]
+        public void PageBar_GoToPreviousPage_Test_1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+
+                testPageBar.GoToPreviousPage(driver);
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToPreviousPage_Test_2()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(testPageBar.GetLastPageNumber, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(0, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(1, testPageBar.NextPageIsNull);
+
+                testPageBar.GoToPreviousPage(driver);
+                testPageBar = new PageBar(driver);
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(testPageBar.GetLastPageNumber - 1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(0, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToPreviousPage_Test_3()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                PageBar testPageBar = new PageBar(driver); 
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(testPageBar.GetLastPageNumber, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(0, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(1, testPageBar.NextPageIsNull);
+
+                while (testPageBar.CurrentPageIsFirst == 0)
+                {
+                    testPageBar.GoToPreviousPage(driver);
+                    testPageBar = new PageBar(driver);
+                }
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+
+            }
+        }
+
+        [TestMethod]
+        public void PageBar_GoToPreviousPage_Test_4()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                /* Initialization */
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://tms.lionbridge.com/");
+
+                string projectTitle = "Qlik";
+                ProjectsPage testPage = new ProjectsPage(driver, projectTitle);
+
+                testPage.ClickChosenProject();
+                ParticularProjectPage qlikPage = new ParticularProjectPage(driver);
+
+                qlikPage.ProfileClick(driver);
+                qlikPage.ChangeItemsPerPage(driver);
+
+                qlikPage.StatusClick(driver);
+                StatusPage qlikStatusPage = new StatusPage(driver);
+
+                PageBar testPageBar = new PageBar(driver);
+                testPageBar.GoToLastPage(driver);
+
+                testPageBar = new PageBar(driver);
+                /* Set of assertions */
+
+                Assert.AreEqual(testPageBar.GetLastPageNumber, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(0, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(1, testPageBar.NextPageIsNull);
+
+                testPageBar.GoToFirstPage(driver);
+                testPageBar = new PageBar(driver);
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+
+                testPageBar.GoToPreviousPage(driver);
+                testPageBar = new PageBar(driver);
+
+                Assert.AreEqual(0, testPageBar.CurrentPageIsNull);
+                Assert.AreEqual(1, testPageBar.CurrentPageIsFirst);
+                Assert.AreEqual(0, testPageBar.CurrentPageIsLast);
+
+                Assert.AreEqual(1, testPageBar.GetCurrentPageNumber);
+                Assert.AreEqual(1, testPageBar.PreviousPageIsNull);
+                Assert.AreEqual(0, testPageBar.NextPageIsNull);
+
+            }
+        }
 
     }
 }
